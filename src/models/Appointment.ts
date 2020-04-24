73cd43a60e47923ef1b539aef2f0e7ab5ec8c652
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-04-23 23:32:28 -0300

    feat: appointment model

---
+++ src/models/Appointment.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+}
+
+export default Appointment;
