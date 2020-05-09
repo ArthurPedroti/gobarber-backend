04e1242b17b8ab99d2fe836f6c0022318a2fbe97
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-05-08 21:20:31 -0300

    refactor: linkov substitution principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,9 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
   public async findByDate(date: Date): Promise<Appointment | null> {
     const findAppointment = await this.findOne({
       where: { date },
