a1883bf0fa4aa56b92fe3ed0fbaf38c490d47c29
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-05-17 12:09:46 -0300

    feat: amazonses configuration

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@acervogeek.com.br',
+      name: 'Arthur Pedroti',
+    },
+  },
+} as IMailConfig;
