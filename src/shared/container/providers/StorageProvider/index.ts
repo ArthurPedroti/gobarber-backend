2d12b2bee394ba22c280a498148baade4a015019
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-05-18 07:44:30 -0300

    refactor: container organization

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,14 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers.disk,
+);
